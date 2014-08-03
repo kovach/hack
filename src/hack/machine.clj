f015c76b894f805c2a84d5bb13dc31da1bbcb794
Scott Kovach <dskovach@gmail.com>
2014-08-02 22:13:58 -0700

    sync

--- src/hack/machine.clj
+++ src/hack/machine.clj
@@ -8,24 +8,40 @@
   )
 
 
-(defn do-op [[ops stack]]
-  (if (seq ops)
-    (let [[op & ops] ops]
-      [ops
-       (case op
-        . (conj stack (first stack))
-        - (rest stack)
-        ; Default, interpet as name
-        (let [name op
-              top (first stack)]
-          (conj (rest stack) (get top name)))
-        )])))
+(defn do-op [history]
+  (let [[[stack ops] & past] history]
+    (if (seq ops)
+      (let [[op & ops-tail] ops]
+        (cons
+          (case op
+             ; dup
+             . [(conj stack (first stack)) ops-tail]
+             ; drop
+             - [(rest stack) ops-tail]
+
+
+             ; Default, interpet as name
+             (let [name op
+                   top (first stack)]
+               [(conj (rest stack) (get top name)) ops-tail])
+             )
+           
+          history))
+      history
+      )))
+
+(def ptest1
+  l1 (cons head (x) tail (nil))
+  a (t1 a0 (t2) (t3 ))
+  '[mod a t1 a0 t2 ]
+  )
 
 (def stack1
-  '({:a 0 :b 22 :c {:d :a}})
+  '({:a 0 :c {:d :a}})
   )
-(-> [['. :c '-] stack1]
-  (do-op)
-  (do-op)
-  (do-op)
-  )
+(-> [[stack1 ['. :c :d '-]]]
+    (do-op)
+    (do-op)
+    (do-op)
+    first
+    )
